3994c18f4b62cfb8169655811b4916aef294caa8
vishnuSekhar <vishnusekhar96@gmail.com>
2023-02-14 19:26:57 +0530

    Initial commit

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  list = [
+    {
+      id: '1',
+      name: 'Home',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      id: '2',
+      name: 'Analytics',
+      icon: 'fa-solid fa-chart-line',
+    },
+    {
+      id: '3',
+      name: 'Products',
+      icon: 'fa-solid fa-box',
+    },
+    {
+      id: '4',
+      name: 'Order',
+      icon: 'fa-solid fa-cart-shopping',
+    },
+    {
+      id: '5',
+      name: 'Settings',
+      icon: 'fa-solid fa-gear',
+    },
+    {
+      id: '6',
+      name: 'About',
+      icon: 'fa-solid fa-circle-info',
+    },
+    {
+      id: '7',
+      name: 'Contact',
+      icon: 'fa-solid fa-phone',
+    },
+  ];
+}
